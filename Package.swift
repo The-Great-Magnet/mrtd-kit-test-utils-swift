aedb77bd65ee468c0451100b7d78d508a156add5
Greg Raine <greg@greatmagnet.co.uk>
2020-04-24 11:55:01 +0000

    Added boilerplate files

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "MRTDKitTestUtilities",
+    name: "MRTDKitTestUtils",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "MRTDKitTestUtilities",
-            targets: ["MRTDKitTestUtilities"]),
+            name: "MRTDKitTestUtils",
+            targets: ["MRTDKitTestUtils"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "MRTDKitTestUtilities",
+            name: "MRTDKitTestUtils",
             dependencies: []),
         .testTarget(
-            name: "MRTDKitTestUtilitiesTests",
-            dependencies: ["MRTDKitTestUtilities"]),
+            name: "MRTDKitTestUtilsTests",
+            dependencies: ["MRTDKitTestUtils"]),
     ]
 )
--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-# MRTDKitTestUtilities
+![MRTD Kit](logo.png) 
 
-A description of this package.
+# MRTD Kit: Test Utilities (Swift)
+
+[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+
+MRTD Kit is a library that allows the validation of Machine Readable Travel Documents against the specification defined by the ICAO 9303 document series.
+
+This repository contains utilities to be used in unit and integration testing the various modules.
+    
